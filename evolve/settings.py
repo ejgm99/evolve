f00bf8ade6ed450d725d01bcd955a62923ed41b8
Ubuntu <ubuntu@ip-172-31-28-119.us-east-2.compute.internal>
2018-07-18 02:32:43 +0000

    configured django to allow amazon hosting

--- evolve/settings.py
+++ evolve/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=xh2^!0&o&lzh$@@4$(hxaob-9n!%44%x^f!wm8h-k=mkjc$0-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ec2-18-191-11-121.us-east-2.compute.amazonaws.com"]
 
 
 # Application definition
